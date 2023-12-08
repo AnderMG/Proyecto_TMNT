236610d1d89c1e2049922ef2f36a6a523155701f
mikel.prieto <mikel.prieto@192.168.5.170>
2023-12-08 17:08:00 +0100

    base de datos y paquete de conexion

---
+++ src/conexion/Conexion.java
@@ -0,0 +1,37 @@
+package conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+
+		Connection cx=null;
+		
+		public	Connection conectar() {
+			try {
+				Class.forName("org.sqlite.JDBC");
+				cx=DriverManager.getConnection("jdbc:sqlite:basedatos.db");
+				System.out.println("Conexion aceptada");
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+			return cx;
+		}
+		
+		public void desconectar() {
+			try {
+				cx.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		public static void main(String [] args) {
+			
+			Conexion cx=new Conexion();
+			cx.conectar();
+		}
+}
